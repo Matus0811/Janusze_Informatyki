77b38dbe359a2cd7478751c7a05b0a174354df56
Pawel Raciborski <wojak2845@gmail.com>
2024-03-14 19:09:45 +0100

    Commit[5]: Poprawki w BeanConfiguration i ProjectManagementSystemApplication

--- back/project-management-system/src/main/java/com/project/projectmanagementsystem/ProjectManagementSystemApplication.java
+++ back/project-management-system/src/main/java/com/project/projectmanagementsystem/ProjectManagementSystemApplication.java
@@ -2,13 +2,6 @@ package com.project.projectmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class ProjectManagementSystemApplication {
--- back/project-management-system/src/main/java/com/project/projectmanagementsystem/configuration/BeanConfiguration.java
+++ back/project-management-system/src/main/java/com/project/projectmanagementsystem/configuration/BeanConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
